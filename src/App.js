34f6cdaef1fed9f53bcadc015af74bcc72d53692
Sxy623 <191436989@qq.com>
2021-01-21 16:52:44 +0800

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Clock from './Clock';
+
+function App() {
+  return (
+      <div>
+          <h1>Hello world!</h1>
+          <Clock />
+          <hr />
+          <p>© 2021 Xinyi Shen.</p>
+          <p>18888921102 | shenxinyi@zju.edu.cn</p>
+      </div>
+  );
+}
+
+export default App;
