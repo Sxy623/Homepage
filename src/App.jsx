02d15662e8e4f33c0b0c4eb3937714b8a5d5adef
Sxy623 <191436989@qq.com>
2021-01-22 18:04:08 +0800

    Tic tac toe

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,28 @@ import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-ro
 import "./App.css";
 import Home from "./Home";
 import InfoTable from "./InfoTable";
+import Game from "./Game";
 
 export default function App() {
   return (
     <Router>
-      <Link to="/">Home</Link>{" | "}
-      <Link to="/infotable">Info Table</Link>
       <Switch>
-        <Route path="/infotable">
-          <Redirect to="/infotable" />
+        <Route path="/info-table">
+          <Redirect to="/info-table" />
           <InfoTable />
+        </Route>
+        <Route path="/tic-tac-toe">
+          <Redirect to="/tic-tac-toe" />
+          <Game />
         </Route>
         <Route path="/">
           <Redirect to="/" />
           <Home />
         </Route>
       </Switch>
+      <Link to="/">Home</Link>{" | "}
+      <Link to="/info-table">Info Table</Link>{" | "}
+      <Link to="/tic-tac-toe">Tic Tac Toe</Link>
     </Router>
   );
 }
