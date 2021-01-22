6d78cb47c29b482e32375246ff9e5d544df63db0
Sxy623 <191436989@qq.com>
2021-01-22 15:38:44 +0800

    Extract Home from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 import "./App.css";
-import Clock from "./Clock";
+import Home from "./Home";
 import InfoTable from "./InfoTable";
 
 export default function App() {
   return (
-    // <div>
-    //   <h1>Hello world!</h1>
-    //   <Clock />
-    //   <hr />
-    //   <InfoTable />
-    //   <hr />
-    //   <p>© 2021 Xinyi Shen.</p>
-    //   <p>18888921102 | shenxinyi@zju.edu.cn</p>
-    // </div>
-
     <Router>
+      <Link to="/">Home</Link>{" | "}
+      <Link to="/infotable">Info Table</Link>
       <Switch>
         <Route path="/infotable">
+          <Redirect to="/infotable" />
           <InfoTable />
         </Route>
         <Route path="/">
           <Redirect to="/" />
-          <div>
-            <h1>Hello world!</h1>
-            <Clock />
-            <hr />
-            <InfoTable />
-            <hr />
-            <p>© 2021 Xinyi Shen.</p>
-            <p>18888921102 | shenxinyi@zju.edu.cn</p>
-          </div>
+          <Home />
         </Route>
       </Switch>
     </Router>
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Clock from "./Clock";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <Clock />
+      <hr />
+      <p>© 2021 Xinyi Shen.</p>
+      <p>18888921102 | shenxinyi@zju.edu.cn</p>
+    </div>
+  );
+}
