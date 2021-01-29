43e962ed40a8d894ff8f24fa4b8e6a2287430fa1
Sxy623 <191436989@qq.com>
2021-01-29 21:02:03 +0800

    Show tips when incorrect

--- src/Conjugacion.jsx
+++ src/Conjugacion.jsx
@@ -6,12 +6,15 @@ import Button from '@material-ui/core/Button';
 function FormTextField(props) {
 
   const handleChange = (event) => {
-    props.onChange(props.index, event.target.value) ;
+    props.onChange(props.index, event.target.value);
   };
+
+  const error = props.status === false;
+  const helperText = props.status ? "" : "Incorrect.";
 
   return (
     <div className="conjugacion-text-field">
-      <TextField id="standard-basic" label={props.label} value={props.value} onChange={handleChange} />
+      <TextField error={error} id="standard-basic" label={props.label} value={props.value} onChange={handleChange} helperText={helperText} />
     </div>
   );
 }
@@ -20,7 +23,7 @@ function FormTextFieldRow(props) {
 
   const row = props.labels.map((label, order) => {
     let index = props.indices[order]
-    return <FormTextField key={order} label={label} index={index} value={props.formValues[index]} onChange={props.onChange} />
+    return <FormTextField key={order} label={label} index={index} value={props.formValues[index]} status={props.status[index]} onChange={props.onChange} />
   });
 
   return (
@@ -35,7 +38,9 @@ class Conjugacion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formValues: Array(6).fill("")
+      formValues: Array(6).fill(""),
+      answers: ["hablo", "hablas", "habla", "hablamos", "habláis", "hablan"],
+      status: Array(6).fill(true)
     }
   }
 
@@ -47,6 +52,22 @@ class Conjugacion extends React.Component {
     });
   }
 
+  checkAnswer() {
+    let status = [];
+    const answers = this.state.answers;
+    const formValues = this.state.formValues;
+    for (let i = 0; i < answers.length; i++) {
+      if (answers[i] === formValues[i]) {
+        status.push(true)
+      } else {
+        status.push(false)
+      }
+    }
+    this.setState({
+      status: status
+    });
+  }
+
   render() {
     let question = "hablar"
     return (
@@ -54,11 +75,23 @@ class Conjugacion extends React.Component {
         <h1>{question}</h1>
         <form noValidate autoComplete="off">
           <div className="conjugacion-form-box">
-            <FormTextFieldRow labels={["Yo", "Tú", "Él, ella, usted"]} indices={[0, 1, 2]} formValues={this.state.formValues} onChange={(index, value) => this.handleChange(index, value)}/>
-            <FormTextFieldRow labels={["Nosotros, -as", "Vosotros, -as", "Ellos, ellas, ustedes"]} indices={[3, 4, 5]} formValues={this.state.formValues} />
+            <FormTextFieldRow
+              labels={["Yo", "Tú", "Él, ella, usted"]}
+              indices={[0, 1, 2]}
+              formValues={this.state.formValues}
+              status={this.state.status}
+              onChange={(index, value) => this.handleChange(index, value)}
+            />
+            <FormTextFieldRow
+              labels={["Nosotros, -as", "Vosotros, -as", "Ellos, ellas, ustedes"]}
+              indices={[3, 4, 5]}
+              formValues={this.state.formValues}
+              status={this.state.status}
+              onChange={(index, value) => this.handleChange(index, value)}
+            />
           </div>
           <div className="conjugacion-button">
-            <Button variant="outlined" color="primary" onClick={() => console.log(this.state)}>确认</Button>
+            <Button variant="outlined" color="primary" onClick={() => this.checkAnswer()}>确认</Button>
           </div>
         </form>
       </div>
