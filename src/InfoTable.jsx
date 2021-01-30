73e9605c66f0c9d8a99297fe4d8514396bab775f
Sxy623 <191436989@qq.com>
2021-01-31 01:18:07 +0800

    Fix ESLint problems

--- src/InfoTable.jsx
+++ src/InfoTable.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-import axios from "axios";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
+import React from 'react'
+import axios from 'axios'
+import IconButton from '@material-ui/core/IconButton'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 class Item extends React.Component {
-
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       isHidden: false
-    };
+    }
   }
 
   hide = () => {
@@ -18,9 +17,9 @@ class Item extends React.Component {
     })
   }
 
-  render() {
+  render () {
     if (this.state.isHidden) {
-      return null;
+      return null
     } else {
       return (
         <tr>
@@ -32,74 +31,73 @@ class Item extends React.Component {
             </IconButton>
           </td>
         </tr>
-      );
+      )
     }
   }
 }
 
-function List(props) {
+function List (props) {
   const listItems = props.items.map((item, index) =>
     <Item key={index} name={item.name} age={item.age} />
-  );
+  )
   return (
     <table>
       <thead>
         <tr>
           <th>Name</th>
           <th>Age</th>
-          <th></th>
+          <th />
         </tr>
       </thead>
       <tbody>
         {listItems}
       </tbody>
     </table>
-  );
+  )
 }
 
 class InfoTable extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.api = "http://39.107.66.30:3000/mock/11/test";
-    this.state = null;
+  constructor (props) {
+    super(props)
+    this.api = 'http://39.107.66.30:3000/mock/11/test'
+    this.state = null
   }
 
-  componentDidMount() {
+  componentDidMount () {
     axios.get(this.api)
       .then(response => {
         this.setState({
           response: response
         })
-        console.log(response);
+        console.log(response)
       })
       .catch(error => {
-        console.log(error);
-      });
+        console.log(error)
+      })
   }
 
-  render() {
+  render () {
     if (this.state?.response?.data?.status === 400) {
       return (
         <div>
           <h3>Failed!</h3>
         </div>
-      );
+      )
     } else if (this.state?.response?.data?.status === 200) {
       return (
         <div>
           <h3>Success!</h3>
           <List items={this.state?.response?.data?.data} />
         </div>
-      );
+      )
     } else {
       return (
         <div>
           <h3>Loading ...</h3>
         </div>
-      );
+      )
     }
   }
 }
 
-export default InfoTable;
+export default InfoTable
